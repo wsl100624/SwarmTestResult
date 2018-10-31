4c735d68b7046b15b87214f75daccd32e7b6ff08
Shilong Wang <wang3027@stthomas.edu>
2018-10-30 20:14:32 -0500

    create redis cluster script

---
+++ Script/redisCluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+  
+REDIS_CONFIG='port 6379
+cluster-enabled yes
+cluster-config-file nodes.conf
+cluster-node-timeout 5000
+appendonly yes'
+
+network=host
+num_replicas=4
+
+#docker network create --driver overlay $network
+
+docker service create --name redis \
+  --network $network \
+  --replicas=$num_replicas \
+  -e REDIS_CONFIG="$REDIS_CONFIG" \
+  -e REDIS_CONFIG_FILE="/usr/local/etc/redis/redis.conf" \
+  redis:5.0 sh -c 'mkdir -p $(dirname $REDIS_CONFIG_FILE) && echo "$REDIS_CONFIG" > $REDIS_CONFIG_FILE && cat $REDIS_CONFIG_FILE && redis-server $REDIS_CONFIG_FILE'
